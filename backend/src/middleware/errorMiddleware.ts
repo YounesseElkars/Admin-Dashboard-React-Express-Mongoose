6868884103fc024933b8b780b81850140dae066d
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-15 16:09:17 +0100

    feat : error middleware for handling errors

---
+++ backend/src/middleware/errorMiddleware.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from 'express';
+
+const notFound = (req: Request, res: Response, next: NextFunction) => {
+  const error = new Error(`Not Found - ${req.originalUrl} `);
+  res.status(404);
+  next(error);
+};
+
+const errrorHandler = (err: Error & { kind: string }, req: Request, res: Response, next: NextFunction) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    statusCode = 404;
+    message = 'Resource not found';
+  }
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+};
+
+export { notFound, errrorHandler };
