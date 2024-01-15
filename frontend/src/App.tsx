9382516f2e7d747fe1790c74c38697349aea7e12
Unesdevdev <y.ounesseelkars9@gmail.com>
2024-01-15 23:03:57 +0100

    setup react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,11 @@
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/sections/Navbar';
-import Container from './components/ui/Container';
 
 const App = () => {
   return (
     <>
       <Navbar />
-
-      {/* Content */}
-      <div className="h-16 w-full">
-        <Container>
-          <h1 className="w-full text-center text-3xl font-semibold">Express - Redux App</h1>
-        </Container>
-      </div>
+      <Outlet />
     </>
   );
 };
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ThemeProvider } from '@/components/theme-provider';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+import Home from './components/sections/Home.tsx';
+import About from './components/sections/About.tsx';
+import Login from './components/sections/Login.tsx';
+import Register from './components/sections/register.tsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" index={true} element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
